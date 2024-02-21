01d28ef4f7e18a297b7763505eec3cc6cf1d9ff3
Ania Bieniecka <annabieniecka5@gmail.com>
2024-02-21 14:52:41 +0100

    Creating models Category, Product, ErrorViewModel

---
+++ ItemsForHome.Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ItemsForHomeStore.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
